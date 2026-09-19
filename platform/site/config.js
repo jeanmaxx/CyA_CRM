2ac9175816dae5bf7cf4d0c2305ca089e65c330c
jeanmaxx <69431920+jeanmaxx@users.noreply.github.com>
2026-09-19 15:18:53 -0600

    Cleanup: retira residuo post-migración

--- platform/site/config.js
+++
@@ -1,4 +0,0 @@
-window.ALVA_SITE_CONFIG={
-  leadFunctionUrl:'https://ibhgisndtaclvwznqugu.supabase.co/functions/v1/platform-lead',
-  publishableKey:'sb_publishable_grQYYOgYg0WR9gmn3QBHpg_UyieIrZ8'
-};
